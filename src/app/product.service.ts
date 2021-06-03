42f7f79beb3ccd2c4d99034c303840bf3789ec8f
joraleig <joraleig@cisco.com>
2021-06-03 08:58:17 -0500

    Added product details

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {map} from 'rxjs/operators';
 import 'rxjs/add/operator/map';
+import 'rxjs/observable'
+import {Observable} from 'rxjs/Observable';
+import {Album} from './album';
 
 @Injectable()
 export class ProductService {
@@ -10,8 +13,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).pipe(map(response => response.json()))
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).pipe(map(response => response.json() as Album));
   }
 
 }
